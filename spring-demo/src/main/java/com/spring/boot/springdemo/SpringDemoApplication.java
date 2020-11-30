632af8d8cb6db17d94a2b0a8e7d270ab863e7391
Ramanuja <ramanujareddy@gmail.com>
2020-11-30 15:18:00 +0700

    Demo Spring Boot Application

--- spring-demo/src/main/java/com/spring/boot/springdemo/SpringDemoApplication.java
+++ spring-demo/src/main/java/com/spring/boot/springdemo/SpringDemoApplication.java
@@ -2,12 +2,20 @@ package com.spring.boot.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "Friend") String name) {
+		return String.format("Hello %s, Welcome To Spring Boot World!", name);
+	}
 }
